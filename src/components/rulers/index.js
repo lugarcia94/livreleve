d9cf44060a7d138553a8915839900ece8df5be62
Lucas Garcia <lucas.garcia@auaha.com.br>
2018-12-10 17:03:59 -0200

    iniciando footer sem vitrine

--- src/components/rulers/index.js
+++ src/components/rulers/index.js
@@ -1,25 +1,26 @@
 import './style.styl';
 
+if (window.innerWidth < 1100) {
+    const rulers = $('.info__list');
 
-const rulers = $('.rulers__list');
-
-rulers.slick({
-    slidesToShow: 3,
-    slidesToScroll: 3,
-    responsive: [
-        {
-            breakpoint: 650,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2
+    rulers.slick({
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        responsive: [
+            {
+                breakpoint: 650,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },{
+                breakpoint: 530,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
             }
-        },{
-            breakpoint: 530,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-            }
-        }
-    ]
+        ]
 
-});
+    });
+}
