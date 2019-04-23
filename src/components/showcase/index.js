73a57a2b02a11c2189befa2c34c08eacd00c703d
Lucas Correia <lucas.correia@auaha.com.br>
2019-04-23 17:52:55 -0300

    #14889 - Implementação Home

--- src/components/showcase/index.js
+++ src/components/showcase/index.js
@@ -15,3 +15,42 @@ $('.showcase').each(function(){
         .addClass('showcase__title')
         .addClass('showcase__title--actived');
 });
+
+$(".showcase-tabs .helperComplement").remove();
+
+$('.showcase-tabs .showcase .showcase__title + ul').slick({
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    infinite: false,
+    responsive: [   
+        {
+            breakpoint    : 992,
+            settings      : {
+                slidesToShow: 3,
+                slidesToScroll: 3
+            }
+        },
+        {
+            breakpoint    : 768,
+            settings      : {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint    : 500,
+            settings      : {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+});
+
+$(".showcase-tabs__tabs-item").click(function() {
+    $(".showcase-tabs__tabs-item").removeClass("showcase-tabs__tabs-item--active")
+    $(this).addClass("showcase-tabs__tabs-item--active")
+    const showcase = "."+$(this).attr("data-showcase")
+    $(".showcase-tabs__showcase .showcase").removeClass("showcase--active")
+    $(showcase).addClass("showcase--active")
+});
