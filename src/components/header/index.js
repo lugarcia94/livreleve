c6a8ce814a2af0e19d8ac58a01cf35fde73fefd9
Lucas Correia <lucas.correia@auaha.com.br>
2019-05-15 12:26:27 -0300

    #14509 - Implementação

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,8 +1,1 @@
 import './index.styl';
-
-$(document).ready(function() {
-    console.log($( "input.fulltext-search-box" ).width())
-    $( "input.fulltext-search-box" ).resize(function() {
-        console.log($( "input.fulltext-search-box" ).width())
-    });
-});
